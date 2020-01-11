b384c7db627ff3b651c6a04fbc8d31c5e6614994
alpha2048 <alpha2048.jp@gmail.com>
2020-01-11 16:08:58 +0900

    add component rough style

--- src/component/donateCard.js
+++ src/component/donateCard.js
@@ -5,9 +5,42 @@ import image_id1 from '../../public/images/baan-kru-noi.jpg';
 
 const style_layout = {
   backgroundColor: 'aqua',
-  width: 600,
-  height: 400,
-  borderRadius: 4 };
+  width: 450,
+  height: 300,
+  borderRadius: 4,
+};
+
+const style_image = {
+  maxWidth: '100%',
+  height: 'auto',
+  width: 'auto',
+};
+
+const style_description = {
+  display: 'flex',
+  flexDirection: 'row',
+  padding: 16,
+  justifyContent: 'space-between',
+  alignItems: 'center',
+};
+
+const style_description__text = {
+  fontWeight: 600,
+  color: 'black',
+  fontSize: 14,
+};
+
+const style_description__button = {
+  border: '1px solid blue',
+  color: 'blue',
+  fontSize: 14,
+  backgroundColor: 'white',
+  paddingLeft: 8,
+  paddingRight: 8,
+  paddingTop: 4,
+  paddingBottom: 4,
+  fontWeight: 600,
+};
 
 export default class DonateCard extends Component {
   constructor(props) {
@@ -17,9 +50,15 @@ export default class DonateCard extends Component {
   render() {
     return (
       <div style={style_layout}>
-        <img src={image_id1} alt="Logo" />
-        <div>
-          test
+        <img style={style_image} src={image_id1} alt="Logo" />
+        <div style={style_description}>
+          <div style={style_description__text}>
+            test of test
+          </div>
+
+          <div style={style_description__button}>
+            donate
+          </div>
         </div>
       </div>
     );
