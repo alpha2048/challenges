327b7e14f5c0e00b7ae818239502ee6300cd0176
alpha2048 <alpha2048.jp@gmail.com>
2020-01-14 13:04:23 +0900

    fix transaction in charity credit_amount

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -2,6 +2,9 @@ class Charity < ActiveRecord::Base
   validates :name, presence: true
 
   def credit_amount(amount)
-    update_column :total, total + amount
+    Charity.transaction do
+      Charity.lock.find(id)
+      update_column :total, total + amount
+    end
   end
 end
